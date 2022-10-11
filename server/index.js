81d248b52f9c92a277fee95a4156efa48a85010b
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-10-11 18:08:19 -0500

    admit changes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 //the '/api' is an endpoint that will be used to connect to the server to fetch data from the server
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
